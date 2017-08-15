19882ce5c1d66670a6a3d13c6be8fe12784c827f
gerald.p <germicseb@hotmail.com>
2017-08-15 09:31:31 +0200

    added db testcase.

--- src/test/DbConnectionTest.java
+++ src/test/DbConnectionTest.java
@@ -1,4 +1,5 @@
-import org.h2.tools.DeleteDbFiles;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,32 +7,35 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class DbConnectionTest {
-    public static void main(String[] args)
-    {
-        try
-        {
+    public static void main(String[] args) {
+        try {
             Class.forName("org.h2.Driver");
-            Connection con = DriverManager.getConnection("jdbc:h2:~/test", "test", "" );
+            Connection con = DriverManager.getConnection("jdbc:h2:~/test", "test", "");
             Statement stmt = con.createStatement();
             //stmt.executeUpdate( "DROP TABLE table1" );
-            stmt.executeUpdate( "CREATE TABLE IF NOT EXISTS table1 ( user varchar(50) )" );
-            stmt.executeUpdate( "INSERT INTO table1 ( user ) VALUES ( 'Claudio' )" );
-            stmt.executeUpdate( "INSERT INTO table1 ( user ) VALUES ( 'Bernasconi' )" );
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS table1 (id bigint auto_increment, day varchar(10), start varchar(20), end varchar(20) )");
+            stmt.executeUpdate("INSERT INTO table1(day,start,end) VALUES ( 'Montag', '10:00','12:00' )");
+            stmt.executeUpdate("INSERT INTO table1(day,start,end) VALUES ( 'Dienstag', '11:00','12:00' )");
 
             ResultSet rs = stmt.executeQuery("SELECT * FROM table1");
-            while( rs.next() )
-            {
-                String name = rs.getString("user");
-                System.out.println( name );
+
+            ObservableList<ObservableList> data = FXCollections.observableArrayList();
+            while (rs.next()) {
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                data.add(row);
+                System.out.println("Row added " + row);
             }
+            System.out.println(data);
             stmt.execute("DROP ALL OBJECTS");
             stmt.close();
             con.close();
 
-        }
-        catch( Exception e )
-        {
-            System.out.println( e.getMessage() );
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
---
+++ src/util/DataBase.java
@@ -0,0 +1,11 @@
+package util;
+
+public class DataBase {
+
+    // auxiliary class
+    private DataBase(){}
+
+    public static void saveOnClose() {
+
+    }
+}
